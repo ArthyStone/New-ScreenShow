506adcad6e8697a4b018877dd9c050c61f3eaad7
ArthyStone <arthur.louison78@gmail.com>
2026-02-22 19:12:36 +0100

    ajout de fonctionnement avec Layout

--- app/Core/View.php
+++ app/Core/View.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 namespace App\Core;
 class View {
     public static function render(string $view, array $data = []): void {
-        $viewFile = __DIR__ . '/../Views/' . $view . '.php';
+        $viewFile = __DIR__ . '/../Views/pages/' . $view . '.php';
 
         if (!file_exists($viewFile)) {
-            throw new \RuntimeException("View {$view} introuvable");
+            throw new \RuntimeException("View $view introuvable");
         }
+        extract($data);
 
-        extract($data, EXTR_SKIP);
-
+        // Capture le contenu de la vue dans $content
+        ob_start();
         require $viewFile;
+        $title = ucfirst($view) ?? "ScreenShow";
+        $content = ob_get_clean();
+
+        // Inclut le layout principal
+        require __DIR__ . '/../Views/layout/layout.php';
     }
 }
